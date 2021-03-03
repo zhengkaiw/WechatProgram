1e3bb054574299e5618ee5349e851b1a94bdd895
kevinZ <zhengkaiw851@gmail.com>
2021-03-03 23:27:50 +0800

    page with js

--- wechat-miniprogram-demo/pages/home/home.js
+++ wechat-miniprogram-demo/pages/home/home.js
@@ -1,1 +1,26 @@
-Page({});
+Page({
+    data: {
+        name: 'Jeff'
+    },
+    buttonHandler(event) {
+        const that = this;
+        wx.showModal({
+            title: 'Need confirm',
+            content: 'Are you sure the change?',
+            success (res) {
+                if (res.confirm) {
+                    that.setData({
+                        name: 'Tom'
+                    }, function() {
+                        wx.showToast({
+                            title: 'complete',
+                            duration: 700
+                        });
+                    });
+                } else if (res.cancel) {
+                    console.log('User canceled');
+                }
+            }
+        })
+    }
+});
