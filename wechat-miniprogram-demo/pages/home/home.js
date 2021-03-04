956728b0cea929610f2e759fad7f9b6e21ecfac4
kevinZ <zhengkaiw851@gmail.com>
2021-03-04 11:23:00 +0800

    finished study

--- wechat-miniprogram-demo/pages/home/home.js
+++ wechat-miniprogram-demo/pages/home/home.js
@@ -77,5 +77,18 @@ Page({
     //         }
     //     });
     // }
-    
+
+    // data: { name: '' },
+    // buttonHandler(event) {
+    //     if (!event.detail.userInfo) return;
+    //     this.setData({
+    //         name: event.detail.userInfo.nickName
+    //     });
+    // }
+
+    buttonHandler(event) {
+        wx.navigateTo({
+            url: '../second/second'
+        });
+    }
 });
