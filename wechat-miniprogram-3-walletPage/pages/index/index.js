14aa673748ec2f56b634bdd9e5dbf4d32a9fa836
kevinZ <zhengkaiw851@gmail.com>
2021-03-06 13:52:36 +0800

    finished wallet page

--- wechat-miniprogram-3-walletPage/pages/index/index.js
+++ wechat-miniprogram-3-walletPage/pages/index/index.js
@@ -1,1 +1,19 @@
-Page({});
+Page({
+    data: {
+        array1: [
+            { icon: '/images/top/money.png', text: '收付款' },
+            { icon: '/images/top/balance.png', text: '零钱\n0.00' },
+            { icon: '/images/top/cards.png', text: '银行卡' }
+        ],
+        array2: [
+            { icon: '/images/service/cardRepay.png', text: '信用卡还款' },
+            { icon: '/images/service/mobileTopup.png', text: '手机充值' },
+            { icon: '/images/service/wealth.png', text: '理财通' },
+            { icon: '/images/service/utilities.png', text: '生活缴费' },
+            { icon: '/images/service/qqCoins.png', text: 'Q币充值' },
+            { icon: '/images/service/publicService.png', text: '城市服务' },
+            { icon: '/images/service/charity.png', text: '腾讯公益' },
+            { icon: '/images/service/insurance.png', text: '保险服务' }
+        ]
+    }
+});
